8abec0c6ca22aa2e58bd8ece2d862177191db37a
ashish <20190802091@dypiu.ac.in>
2022-11-06 17:33:28 +0530

    added functionalities

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,21 +1,69 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from "react";
+import useSound from "use-sound";
+import play from "../assets/sounds/play.mp3";
+import correct from "../assets/sounds/correct.mp3";
+import wrong from "../assets/sounds/wrong.mp3";
 
-export default function Trivia({data, setTimeOut, questionNumber, setQuestionNumber}) {
+export default function Trivia({
+  data,
+  setStop,
+  questionNumber,
+  setQuestionNumber,
+}) {
   const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+  const [letsPlay] = useSound(play);
+  const [correctAnswer] = useSound(correct);
+  const [wrongAnswer] = useSound(wrong);
 
-  useEffect(()=>{
-    setQuestion(data[questionNumber-1])
-  }, [data, questionNumber])
+  useEffect(() => {
+    letsPlay();
+  }, [letsPlay]);
 
+  useEffect(() => {
+    setQuestion(data[questionNumber - 1]);
+  }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () =>
+      setClassName(a.correct ? "answer correct" : "answer wrong")
+    );
+    delay(5000, () => {
+      if (a.correct) {
+        correctAnswer();
+        delay(1000, () => {
+          setQuestionNumber((preNum) => preNum + 1);
+          setSelectedAnswer(null);
+        });
+      } else {
+        wrongAnswer();
+        delay(1000, () => {
+          setStop(true);
+        });
+      }
+    });
+  };
   return (
-    <div className='trivia'>
-        <div className="question">{question?.question}</div>
-        <div className="answers">
-        {question?.answers.map(a=>(
-          <div className="answer">{a.text}</div>
+    <div className="trivia">
+      <div className="question">{question?.question}</div>
+      <div className="answers">
+        {question?.answers.map((a) => (
+          <div
+            className={selectedAnswer === a ? className : "answer"}
+            onClick={() => handleClick(a)}
+          >
+            {a.text}
+          </div>
         ))}
-            
-        </div>
+      </div>
     </div>
-  )
+  );
 }
